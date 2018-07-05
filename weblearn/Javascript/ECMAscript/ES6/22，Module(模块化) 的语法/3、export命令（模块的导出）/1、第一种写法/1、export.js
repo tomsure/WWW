df952a60919fa3f359751960a69a8f761df97aba
tomsure <491583156@qq.com>
2018-07-05 23:12:45 +0800

    模块的导出

---
+++ weblearn/Javascript/ECMAscript/ES6/22，Module(模块化) 的语法/3、export命令（模块的导出）/1、第一种写法/1、export.js
@@ -0,0 +1,6 @@
+//		export命令用于导出当前文件中其他地方需要使用的模块，这个命令和import命令配合使用，import用于导入模块
+        export var a=123;
+        export var b=456;
+        export var c=789
+
+//  这里声明方式的写法是在ES5声明变量的基础上，声明每个变量的时候，在声明语句之前加上export
---
+++ weblearn/Javascript/ECMAscript/ES6/22，Module(模块化) 的语法/fn.js
@@ -0,0 +1,18 @@
+function aa(){
+	console.log('123')
+}
+
+function bb(){
+	console.log(456)
+}
+
+function cc(){
+	console.log(789)
+}
+
+export={
+	a:aa,
+	b:bb,
+	c:cc
+}
+
