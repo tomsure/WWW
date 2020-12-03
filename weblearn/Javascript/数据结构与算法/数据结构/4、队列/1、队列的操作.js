49cfbb32f9c7a696734af57c10d660a013bef16a
tangxu <491583156@qq.com>
2020-12-03 23:05:46 +0800

    算法

---
+++ weblearn/Javascript/数据结构与算法/数据结构/4、队列/1、队列的操作.js
@@ -0,0 +1,6 @@
+console.log(11)
+console.log(11)
+var qq = 111
+let aa = []
+aa.push('111')
+console.log('111w', aa)
---
+++ weblearn/Javascript/数据结构与算法/数据结构/4、队列/2、一个用数组实现的队列.js
@@ -0,0 +1,4 @@
+names = [];
+names.push("Cynthia");
+names.push("Jennifer");
+console.log(names); // 显示 Cynthia,Jennifer
